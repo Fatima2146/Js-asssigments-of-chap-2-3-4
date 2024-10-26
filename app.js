ee685174839c9f1a9702d5c42ea032244d4070f2
Ayesha Ashraf <fatimaashraf1001@gmail.com>
2024-10-26 16:35:08 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+// CHAPTER 2
+// Question 1.
+
+let username;
+
+// Question 2.
+let myName = "Ayesha Ashraf";
+
+// Question 3.
+let message = alert("Hello World");
+
+// Question 4.
+let studname = alert("Name:  Ayesha");
+let studage = alert("age:  18");
+let studcourse = alert("course:  Web and App Development");
+
+// Question 5.
+let pizza = alert("PIZZA \n PIZZ \n PIZ \n PI \n P");
+
+// Question 6.
+let email = alert("My email address is ayeshashraf@gmail.com");
+
+// Question 7.
+let book = alert("I am trying to learn from the book A smarter way to learn javascript");
+
+// Question 8. 
+let a ;
+document.write("Yah!! I can write HTML content through Javascript");
+
+// Question 9.
+ let symbol = alert("=========(✿◕‿◕✿)===========")
+
+
+
