49409732a667b59150c7ea92917cfa7b2996fb3e
Ayesha Ashraf <fatimaashraf1001@gmail.com>
2024-10-27 13:25:21 +0500

    Add files via upload

---
+++ app4.js
@@ -0,0 +1,21 @@
+// Chapter 4:
+// Question 1.
+let a = 1 , b = 2, c = 3;
+
+// Question 2.
+// legal variables
+let myName ;
+let name1;
+let giga_byte;
+let helloWorld;
+let isVAriaBle;
+// illegal variables
+let @name;
+let goat-Cottage;
+let 123hello;
+let full name ; 
+let class;
+
+// Question 3.
+document.write("Variables can only contain letters , digits, underscore, and dollar sign. for example : $my_1stVariable.", "<br>", "Variables must begin with a letter, $ or _.  for example: $name, _name or name." , "<br>", "Variables names are case sensitive." , "<br>" , "variables names should not be JS keywords." );
+
